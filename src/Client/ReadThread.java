f584105bb0b82520085c8f719709a8c85a7828f9
Nika-Soltani-Tehrani <nsoltanitehrani@gmail.com>
2021-06-14 10:01:53 +0430

    sixth-commit

---
+++ src/Client/ReadThread.java
@@ -0,0 +1,47 @@
+package Client;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ * This thread is responsible for reading server's input and printing it
+ * to the console.
+ * It runs in an infinite loop until the client disconnects from the server.
+ *
+ * @author www.codejava.net
+ */
+public class ReadThread extends Thread {
+    private DataInputStream reader;
+    private Socket socket;
+    private Client client;
+
+    public ReadThread(Socket socket, Client client) {
+        this.socket = socket;
+        this.client = client;
+
+        try {
+            this.reader = new DataInputStream(socket.getInputStream());
+        } catch (IOException ex) {
+            System.out.println("Error getting input stream: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                String response = reader.readUTF();
+                System.out.println("\n" + response);
+
+                // prints the username after displaying the server's message
+                if (client.getUsername() != null) {
+                    System.out.print("[" + client.getUsername() + "]: ");
+                }
+            } catch (IOException ex) {
+                System.out.println("Error reading from server: " + ex.getMessage());
+                ex.printStackTrace();
+                break;
+            }
+        }
+    }
+}
---
+++ src/Client/WriteThread.java
@@ -0,0 +1,80 @@
+package Client;
+
+
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+/**
+ * This thread is responsible for reading user's input and send it
+ * to the server.
+ * It runs in an infinite loop until the user types 'bye' to quit.
+ *
+ * @author www.codejava.net
+ */
+public class WriteThread extends Thread {
+    private DataOutputStream writer;
+    private Socket socket;
+    private Client client;
+    private File file ;
+
+
+    public WriteThread(Socket socket, Client client) {
+        this.socket = socket;
+        this.client = client;
+
+        try {
+
+            this.writer = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException ex) {
+            System.out.println("Error getting output stream: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    public void run() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nEnter your name: ");
+        String userName = sc.nextLine();
+        client.setUsername(userName);
+        file = new File("./" + userName + "File.txt");
+        client.fileWriter(userName + "'s messages\n",file);
+
+        try {
+            writer.writeUTF(userName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String text;
+
+        do {
+            System.out.println("[" + userName + "]: ");
+            text = sc.nextLine();
+
+            if (text.equals("HISTORY"))
+            {
+                client.fileReader(file);
+            }
+            else {
+                client.editFile(file, text + "\n");
+                try {
+                    writer.writeUTF(text);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } while (!text.equals("exit"));
+
+        try {
+            socket.close();
+        } catch (IOException ex) {
+
+            System.out.println("Error writing to server: " + ex.getMessage());
+        }
+    }
+}
